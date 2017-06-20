d72b5e18f94a2099b89c02c4f12320f976c26922
vishalmishra4u <mishravishal0912@gmail.com>
2017-06-20 19:44:42 +0530

    Changes in index.js for bug fixes

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
 var isNumber = require('is-number');
 
 module.exports = {
-  first : function(arr, index2){
-    if(!Array.isArray(arr)){
-      throw new Error('array-n-first expects first arguement as an array.');
-    }
-
-    if(arr.length === 0){
-      return null;
-    }
-
-    var firstN = firstN(arr, 0, isNumber(index2) ? index2 : 0);
-
-    return firstN;
-  }
-
-  midN : function(arr, index1 = 0, index2){
-    if(!Array.isArray(arr)){
-      throw new Error('array-n-first expects first arguement as an array.');
-    }
-
-    if(arr.length === 0){
-      return null;
-    }
-
-    var firstN = firstN(arr, isNumber(index1) ? index2 : 0, isNumber(index2) ? index2 : 0);
-
-    return firstN;
-  }
+  first : first,
+  midN : midN,
+  getElements : getElements
 };
 
-function firstN(arr, index1, index2){
+function first(arr){
+  if(!Array.isArray(arr)){
+    throw new Error('array-n-first expects first arguement as an array.');
+  }
+
+  if(arr.length === 0){
+    return null;
+  }
+
+  return getElements(arr, 0, 1);
+}
+
+function midN(arr, index1 = 0, index2){
+  if(!Array.isArray(arr)){
+    throw new Error('array-n-first expects first arguement as an array.');
+  }
+
+  if(!isNumber(index1) || isNumber(index2)){
+    throw new Error('Indexes must be an integer');
+  }
+
+  if(arr.length === 0){
+    return null;
+  }
+
+  return getElements(arr, isNumber(index1) ? index2 : 0, isNumber(index2) ? index2 : 0);
+}
+
+function getElements(arr, index1, index2){
   var tempArray = [];
   for(var i = index1; i < index2 + 1;i++){
     tempArray.push(arr[i]);
   }
 
-  return tempArray;
+  return tempArray.length == 1 ? tempArray[0] : tempArray;
 }
